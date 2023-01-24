f78821c1c505b063e417701327de3467983b5734
Rahul-Mishra-creator <77747066+Rahul-Mishra-creator@users.noreply.github.com>
2023-01-24 13:25:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.post("/",function(req,res){
 
 
     
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000,function(){
-    console.log("Server is running on port 3000");
+app.listen(port,function(){
+    console.log("Server is running on port "+port);
 });
 
